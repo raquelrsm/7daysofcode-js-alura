ec624c402540513f00ae6c33749ef6efbed45548
Raquel Rodrigues <66495985+raquelrsm@users.noreply.github.com>
2022-04-01 13:28:38 -0300

    Update and rename main.js to script.js

--- Day02/script.js
+++ Day02/script.js
@@ -7,23 +7,25 @@ function mandarMsg() {
     const linguagem = document.getElementById("input-linguagem").value;
 
     if (nome != "" && idade != "" && linguagem != "") {
-        const msg = `<p class='msg-ola'>Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!</p>
-       <p>Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.</p>
+        const msg = `<p class='msg-ola'>Olá ${nome}! Você tem ${idade} anos e está aprendendo ${linguagem}! Que legal!</p>
+       <p>Você gosta de estudar ${linguagem}? Digite o número 1 para SIM ou 2 para NÃO.</p>
        <input type='number' id='input-numero'><button type='button'onclick='mandarResposta()'>enviar</button>`
 
         divMsg.innerHTML = msg;
     }
 }
+
 function mandarResposta() {
     const numero = document.getElementById("input-numero").value;
     if (numero == 1) {
-        const resposta = "Que legal! Continue estudando que você ficará muito bom!.";
+        const resposta = "Muito bom! Continue aprendendo e estudando para que você possa melhorar suas habilidades cada vez mais.";
         divResposta.innerHTML = resposta;
     } else if (numero == 2) {
-        const resposta = "Sério? Que pena! Já pensou em aprender outras linguagens?";
+        const resposta = "Sério? Você não gosta? Tente aprender outras linguagens de programação!";
         divResposta.innerHTML = resposta;
     } else {
         const resposta = "Resposta inválida";
+
         divResposta.innerHTML = resposta;
     }
 }
